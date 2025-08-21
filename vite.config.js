18dabcce9fce704271355c6bff20f3c81c986060
zaki-ramadhan <zakiram4dhan@gmail.com>
2025-08-21 10:59:08 +0700

    fix: remove unecessary base url in vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/zappify",
   resolve: {
     alias: {
       // meringkas path
